ac7592eeb19aac094794f1f672b8a8ce937efd89
belhajManel <belhaj343@gmail.com>
2023-03-16 06:05:18 -0700

    fix Betty documentation style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main: Entry point
+/**
+ * main - Entry point
  *
- * Description: A program that prints a sentance with print function
+ * Description: A C program that prints with printf function.
  *
- * Return: Always 0
- */
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
