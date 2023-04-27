a1208e87edf1d6c98cb6e15fb902e83dae8e46ef
BELHAJ Manel <belhaj343@gmail.com>
2023-04-27 22:24:42 +0100

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,46 @@
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node_end - adds a new node at the end
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
+ * _strlen - returns the length of a string
+ * @s: string to be measured
+ *
+ * Return: the length of the string
  */
+int _strlen(const char *s)
+{
+	int i = 0;
 
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the head of the list
+ * @str: string to be duplicated
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *xp, *temp;
-size_t numchar;
+	list_t *new, *last;
 
-xp = malloc(sizeof(list_t));
-if (xp == NULL)
-return (NULL);
-
-xp->str = strdup(str);
-
-for (numchar = 0; str[numchar]; numchar++)
-;
-
-xp->len = numchar;
-xp->next = NULL;
-temp = *head;
-
-if (temp == NULL)
-{
-*head = xp;
-}
-else
-{
-while (temp->next != NULL)
-temp = temp->next;
-temp->next = xp;
-}
-
-return (*head);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
+	return (new);
 }
