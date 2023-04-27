e380306beacac2c2f03e5695490d14848a0dab53
BELHAJ Manel <belhaj343@gmail.com>
2023-04-27 22:27:02 +0100

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,20 @@
 #include "lists.h"
 
 /**
- * free_list - Realease the memory allocated for a list
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
  *
- * @head: A pointer to the first node of the list to free
+ * Return: void
  */
 void free_list(list_t *head)
 {
-if (head)
-{
-free_list(head->next);
-if (head->str)
-free(head->str);
-free(head);
-}
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
 }
